f0d84b77423688a0e58e970ef38ce97ce593666f
kauacomk123 <kauasantana2707@gmail.com>
2026-05-18 18:57:57 -0300

    git update, novas funcionalidades no arquivo app.jsx mudanças nos codigos do estados e criamentos de novas function

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
 import { useState } from "react";
 
-function Square(){
-  const [value, setValue] = useState(null);
-
-  function hardleClick(){
-    setValue('X')
-  }
-  
-  return (
-    <button className="square" onClick={hardleClick}>
+function Square({value, onSquareClick}){
+    return (
+    <button className="square" onClick={onSquareClick}>
       {value}</button>
   );
 }
 
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function hardleClick(){
+    const nexSquares = squares.slice();
+    nexSquares[0] = "X";
+    setSquares(nexSquares);
+  }
+
   return ( 
     <>
     <div className="board-row">
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[0]} onSquareClick={hardleClick} />
+     <Square value={squares[1]} onSquareClick={hardleClick} />
+     <Square value={squares[2]} onSquareClick={hardleClick} />
     </div>
 
     <div className="board-row">
-     <Square />
-     <Square />
-     <Square />
+     <Square value={squares[3]} onSquareClick={hardleClick} />
+     <Square value={squares[4]} onSquareClick={hardleClick} />
+     <Square value={squares[5]} onSquareClick={hardleClick} />
     </div>
 
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} onSquareClick={hardleClick}/>
+      <Square value={squares[7]} onSquareClick={hardleClick}/>
+      <Square value={squares[8]} onSquareClick={hardleClick}/>
     </div>
     
     </>
